f199a2c58b1082854d7edacfdf5cf6abfd1b16e2
Cynthia Sosa <cynthiasosa33@iclould.com>
2024-12-03 23:36:06 -0500

    commit

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -4,6 +4,6 @@ if (req.session.lastVisit) {
 console.log(req.session.lastVisit);
 }
 req.session.lastVisit = new Date();
-res.render('index', { title: 'Cynthia Sosa' });
+res.render('index', { title: 'Cynthia Sosaa' });
 
 };
